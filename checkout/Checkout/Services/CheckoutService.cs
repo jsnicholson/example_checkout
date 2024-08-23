c445fc17d43c37ceab43bad5504c934de4e15af9
Joshua Nicholson <jsnicholson08@gmail.com>
2024-08-23 22:07:32 +0100

    add memoisation to improve potential performance with many rules
    noticed that current implementation has a recursive in a foreach (scary!).
    
    could dig deep on this, but works for current situation. added memoisation as a potential improvement

--- checkout/Checkout/Services/CheckoutService.cs
+++ checkout/Checkout/Services/CheckoutService.cs
@@ -15,30 +15,36 @@ namespace Checkout.Services {
             decimal totalPrice = 0;
 
             foreach((string sku, int quantity) in _scannedItems) {
+                Dictionary<(string sku, int quantity), decimal> priceMemoisation = new Dictionary<(string sku, int quantity), decimal>();
                 var applicableRules = _priceRules.Where(r => r.Sku == sku).ToList();
-                totalPrice += GetBestPrice(sku, applicableRules, quantity);
+                totalPrice += GetBestPrice(sku, applicableRules, quantity, priceMemoisation);
             }
 
             return totalPrice;
         }
 
-        private decimal GetBestPrice(string sku, List<PriceRule> priceRules, int quantity) {
-            if(quantity <= 0) return 0;
+        private decimal GetBestPrice(string sku, List<PriceRule> priceRules, int quantity, Dictionary<(string sku, int quantity), decimal> priceMemoisation) {
+            Console.WriteLine($"GetBestPrice called");
+            if (quantity <= 0) return 0;
+
+            if (priceMemoisation.TryGetValue((sku, quantity), out var cachedPrice)) return cachedPrice;
 
             decimal minPrice = decimal.MaxValue;
 
-            foreach(var rule in priceRules) {
+            foreach (var rule in priceRules) {
                 PriceResult result = rule.CalculatePrice(quantity);
 
-                if(result.quantityAccountedFor > 0) {
-                    decimal remainingPrice = GetBestPrice(sku, priceRules, quantity - result.quantityAccountedFor);
+                if (result.quantityAccountedFor > 0) {
+                    decimal remainingPrice = GetBestPrice(sku, priceRules, quantity - result.quantityAccountedFor, priceMemoisation);
                     minPrice = Math.Min(minPrice, result.price + remainingPrice);
                 }
             }
 
-            if(minPrice == decimal.MaxValue) {
+            if (minPrice == decimal.MaxValue) {
                 throw new NotAllIItemsPricedException($"No applicable rules found for {quantity} '{sku}'s");
             }
+
+            priceMemoisation[(sku, quantity)] = minPrice;
 
             return minPrice;
         }
