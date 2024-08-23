5c3a5a5f6a8b06a34da433b78f3b0ce1ef0e5706
Joshua Nicholson <jsnicholson08@gmail.com>
2024-08-23 20:28:06 +0100

    implement unitpricerule

---
+++ checkout/Checkout/Models/UnitPriceRule.cs
@@ -0,0 +1,13 @@
+﻿namespace Checkout.Models {
+    public class UnitPriceRule : PriceRule {
+        private readonly decimal _unitPrice;
+
+        public UnitPriceRule(string sku, int unitPice) : base(sku) {
+            _unitPrice = unitPice;
+        }
+
+        public override decimal CalculatePrice(int quantity) {
+            return _unitPrice * quantity;
+        }
+    }
+}
