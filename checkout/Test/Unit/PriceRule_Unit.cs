ce1662ab7e3ed86c9cff30e93e57bf5c9b2884b6
Joshua Nicholson <jsnicholson08@gmail.com>
2024-08-23 20:31:35 +0100

    implement specialpricerule test

--- checkout/Test/Unit/PriceRule_Unit.cs
+++ checkout/Test/Unit/PriceRule_Unit.cs
@@ -3,7 +3,9 @@
 namespace Test.Unit {
     public class PriceRule_Unit {
         [Theory]
-        [InlineData(0, 0, 0)]
+        [InlineData(10, 3, 30)]
+        [InlineData(5, 4, 20)]
+        [InlineData(1.5, 4, 6)]
         public void UnitPrice_CalculatePrice_Correct(decimal unitPrice, int quantity, decimal expectedTotal) {
             var unitPriceRule = new UnitPriceRule("A", unitPrice);
 
@@ -15,7 +17,11 @@ namespace Test.Unit {
         [Theory]
         [InlineData(0, 0, 0, 0, 0)]
         public void SpecialPrice_CalculatePrice_Correct(int specialPrice, int quantityForSpecialPrice, int fallbackPrice, int quantity, int expectedTotal) {
+            var specialPriceRule = new SpecialPriceRule("A", fallbackPrice, quantityForSpecialPrice, specialPrice);
 
+            var result = specialPriceRule.CalculatePrice(quantity);
+
+            Assert.Equal(expectedTotal, result);
         }
     }
 }
