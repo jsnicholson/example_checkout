59d3aa099ae4a52f22ea75762c7f1cac09206fdf
Joshua Nicholson <jsnicholson08@gmail.com>
2024-08-23 20:40:41 +0100

    add example data for unitprice

--- checkout/Test/Unit/CheckoutService_Unit.cs
+++ checkout/Test/Unit/CheckoutService_Unit.cs
@@ -4,7 +4,8 @@ using Checkout.Services;
 namespace Test.Unit {
     public class CheckoutService_Unit {
         public static IEnumerable<object[]> UnitPriceData => [
-            new object[] { }    
+            [new List<PriceRule> { new UnitPriceRule("A", 10) }, new List<string> { "A", "A", "A" }, 30],
+            [new List<PriceRule> { new UnitPriceRule("A", 50), new UnitPriceRule("B", 30)}, new List<string> { "A", "B", "A"}, 130]
         ];
 
         public static IEnumerable<object[]> SpecialPriceData => [
