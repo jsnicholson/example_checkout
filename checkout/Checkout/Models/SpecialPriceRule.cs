63e03a4e167b836cac638e94db9310f2777d37bb
Joshua Nicholson <jsnicholson08@gmail.com>
2024-08-23 21:21:11 +0100

    add an important comment
    thought on fallback price in special price. not a good pattern, want to avoid

--- checkout/Checkout/Models/SpecialPriceRule.cs
+++ checkout/Checkout/Models/SpecialPriceRule.cs
@@ -2,6 +2,8 @@
     public class SpecialPriceRule : PriceRule {
         private readonly int _quantityForSpecialPrice;
         private readonly decimal _specialPrice;
+        // fallback price is basically a unit price. want to avoid some complex inheritance tree though.
+        // want a way of getting this out, means that price calculation either needs to be aware of other pricing (bad), or we need to return other data?
         private readonly decimal _fallbackPrice;
 
         public SpecialPriceRule(string sku, int quantityForSpecialPrice, decimal specialPrice, decimal fallbackPrice) : base(sku) {
